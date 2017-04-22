e2fe737c0668a936df89ed004e063b58880e64b7
Alisha Kapoor <Alisha Kapoor>
2017-04-22 15:44:29 +0530

    java file

--- app/src/main/java/com/example/rusty/society_shopping/Admin.java
+++ app/src/main/java/com/example/rusty/society_shopping/Admin.java
@@ -2,12 +2,18 @@ package com.example.rusty.society_shopping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Admin extends AppCompatActivity {
-
+    TextView t1;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        t1=(TextView)findViewById(R.id.adm);
+        img=(ImageView)findViewById(R.id.img);
+
     }
 }
--- app/src/main/java/com/example/rusty/society_shopping/pendingorder.java
+++ app/src/main/java/com/example/rusty/society_shopping/pendingorder.java
@@ -2,12 +2,36 @@ package com.example.rusty.society_shopping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class pendingorder extends AppCompatActivity {
+public class pendingorder extends AppCompatActivity implements View.OnClickListener {
 
+    EditText e1,e2,e3,e4,e5;
+    CheckBox check;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pendingorder);
+
+        e1=(EditText)findViewById(R.id.shop);
+        e2=(EditText)findViewById(R.id.customer);
+        e3=(EditText)findViewById(R.id.total);
+        e4=(EditText)findViewById(R.id.address);
+        e5=(EditText)findViewById(R.id.pick);
+
+        check=(CheckBox)findViewById(R.id.check);
+        check.setOnClickListener(this);
+
+        btn=(Button)findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
