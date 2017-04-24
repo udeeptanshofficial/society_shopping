83a52be13077802d5a74f01ee5638ff54856c92f
wplanner00000 <wplanner0000@gmail.com>
2017-04-24 12:05:27 +0530

    dnjcndjc

--- app/src/main/java/com/example/rusty/society_shopping/placeorder.java
+++ app/src/main/java/com/example/rusty/society_shopping/placeorder.java
@@ -1,6 +1,7 @@
 package com.example.rusty.society_shopping;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -35,11 +36,14 @@ public class placeorder extends AppCompatActivity {
     final ArrayList<String> selectedproductname = new ArrayList<>();
     final ArrayList<String> selectedproductprice = new ArrayList<>();
     ArrayAdapter adap;
+    String shopid;
+    String userid;
     ArrayList<String> ar = new ArrayList<>();
     String respons; Spinner listofdata;
     JSONObject obj;
     JSONArray jsonArray;
-
+    SharedPreferences pref;
+    int count[] = new int[20];
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -51,15 +55,32 @@ public class placeorder extends AppCompatActivity {
         Intent intent = getIntent();
         respons = intent.getStringExtra("Society_id");
         setSpinnerAdaptor(respons);
-
+        pref = getSharedPreferences(InitializePref.myPrefrence,this.MODE_PRIVATE);
+        userid = pref.getString("Resident_name","");
         list_additem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                    view.setSelected(true);
-                    view.setBackgroundResource(R.color.colorPrimary);
-                selectedproductname.add(productname.get(position));
-                selectedproductprice.add(productprice.get(position));
+
+                    if (count[position]==1)
+                    {
+                        count[position]=0;
+                        view.setSelected(false);
+                        view.setBackgroundResource(R.color.white);
+                        selectedproductname.remove(productname.get(position));
+                        selectedproductprice.remove(productprice.get(position));
+//                       Toast.makeText(placeorder.this, ""+selectedproductname, Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(placeorder.this, ""+selectedproductprice, Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        count[position]=1;
+                        view.setSelected(true);
+                        view.setBackgroundResource(R.color.colorPrimary);
+                        selectedproductname.add(productname.get(position));
+                        selectedproductprice.add(productprice.get(position));
+//                        Toast.makeText(placeorder.this, ""+selectedproductname, Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(placeorder.this, ""+selectedproductprice, Toast.LENGTH_SHORT).show();
+                    }
             }
         });
 
@@ -83,6 +104,7 @@ public class placeorder extends AppCompatActivity {
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                       try {
                           final String name = obj.getJSONArray("List").getJSONObject(position).getString("username");
+                          shopid = name;
                           StringRequest request = new StringRequest(Request.Method.POST, "https://wplanner.000webhostapp.com/user/productList.php", new Response.Listener<String>() {
                               @Override
                               public void onResponse(String response) {
@@ -92,6 +114,8 @@ public class placeorder extends AppCompatActivity {
                                   try {
                                       JSONObject obj1 = new JSONObject(response);
                                       JSONArray jsonArray = obj1.getJSONArray("product");
+                                      productname.clear();
+                                      productprice.clear();
                                       for (int i = 0; i < jsonArray.length(); i++) {
                                           JSONObject jsonObject = jsonArray.getJSONObject(i);
                                           shopnames.add(jsonObject.getString("productname"));
@@ -152,84 +176,39 @@ public class placeorder extends AppCompatActivity {
         queue.add(request);
 
     }
-    /*public  void addproducts(final String shopid, final String societyid)
-    {
-
-        final ArrayList<String> shopnames = new ArrayList<>();
-        final ArrayList<String> shopaddresss = new ArrayList<>();
-        StringRequest stringRequest;
-        stringRequest = new StringRequest(Request.Method.POST, "",
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("DataBase Response", response);
-                        if (response.equals("fail") == false) {
-                            try {
-                                JSONArray jsonArray = new JSONArray(response);
-                                for(int i=0;i<jsonArray.length();i++) {
-                                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                    shopnames.add(jsonObject.getString("productname"));
-                                    shopaddresss.add(jsonObject.getString("price"));
-                                }
-                                ShoplistAdapter adapter = new ShoplistAdapter(placeorder.this,shopnames,shopaddresss);
-                                list_additem.setAdapter(adapter);
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-
-                            }
-
-
-
-
-                        }
-                        else {
-
-                        }
-                    }
-
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(placeorder.this, "No Internet Connection", Toast.LENGTH_SHORT).show();
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("societyid",societyid);
-                params.put("shopid",shopid);
-                return params;
-            }
-
-        };
-
-        RequestQueue requestQueue = Volley.newRequestQueue(placeorder.this);
-        requestQueue.add(stringRequest);
-    }*/
     public void submit(View v)
     {
+//        Toast.makeText(this, ""+respons, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, ""+userid, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, ""+shopid, Toast.LENGTH_SHORT).show();
         final JSONArray jsonarrayname = new JSONArray(selectedproductname);
         final JSONArray jsonarrayprice = new JSONArray(selectedproductprice);
         StringRequest stringRequest;
-        stringRequest = new StringRequest(Request.Method.POST, "",
+        stringRequest = new StringRequest(Request.Method.POST, "https://wplanner.000webhostapp.com/user/placeorder.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         Log.d("DataBase Response", response);
+                        Toast.makeText(placeorder.this, ""+response, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(placeorder.this, "Order Successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(placeorder.this,booking_orders.class);
+                        startActivity(intent);
+                        finish();
                     }
 
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(placeorder.this, "No Internet Connection", Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(placeorder.this, "No Internet Connection", Toast.LENGTH_SHORT).show();
                     }
                 }) {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
+                params.put("societyid",respons);
+                params.put("userid",userid);
+                params.put("shopid",shopid);
                 params.put("jsonarrayname",jsonarrayname.toString());
                 params.put("jsonarrayprice",jsonarrayprice.toString());
                 return params;
